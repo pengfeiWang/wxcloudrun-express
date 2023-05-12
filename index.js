fd1b1fa7b232d85e66266325df84576ca71a16e3
w.pengfei <>
2023-05-12 16:55:39 +0800

    feat: add subscribeMessage

--- index.js
+++ index.js
@@ -12,7 +12,34 @@ app.use(express.json());
 app.use(cors());
 app.use(logger);
 
+/* 
+
+
+const request = require('request')
+return new Promise((resolve, reject) => {
+  request({
+    method: 'POST',
+    url: 'http://api.weixin.qq.com/cgi-bin/message/custom/send',
+    // 资源复用情况下，参数from_appid应写明发起方appid
+    // url: 'http://api.weixin.qq.com/cgi-bin/message/custom/send?from_appid=wxxxxx'
+    body: JSON.stringify({
+      touser: "OPENID", // 一般是消息推送body的FromUserName值，为用户的openid
+      msgtype: "text",
+      text: {
+        content: "Hello World"
+      }
+    })
+  },function (error, response) {
+    console.log('接口返回内容', response.body)
+    resolve(JSON.parse(response.body))
+  })
+})
+
+*/
+
 const subscribeMessage = (req, res) => {
+  console.log("req");
+  console.log(req);
   res.send({
     code: 200,
     data: { message: "成功" },
